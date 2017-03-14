3735769e77dd224834fa1cfa6bce933924fd20c6
Alexander Steen <post+github@alexandersteen.be>
2017-03-14 15:42:08 +0100

    added haalPokemonOp en geefPokemonWeer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,20 @@
  */
 
 
-var testApi = function () {
-    var pokemonID = 500;
+var geefPokemonWeer = function (naam, hoogte, afbeeldingLink) {
+
+};
+
+var haalPokemonOp = function (e) {
+    e.preventDefault();
+    var pokemonID = $(this).attr('id');
     var url = 'http://pokeapi.co/api/v2/pokemon/' + pokemonID;
     $.getJSON(url, function (data) {
         console.log(data);
-
-
+        var naam = data.name;
+        var hoogte = data.height;
+        var afbeeldingLink = data.sprites.front_default;
+        geefPokemonWeer(naam, hoogte, afbeeldingLink)
     })
 };
 
