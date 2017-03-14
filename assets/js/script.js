f46b7ba4adc643eeac570a63c5256193462b23aa
Alexander Steen <post+github@alexandersteen.be>
2017-03-14 14:47:56 +0100

    added jquery and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 /**
  * Created by alexander on 14/03/2017.
  */
+
+
+var testApi = function () {
+    var pokemonID = 1;
+    var url = 'http://pokeapi.co/api/v2/pokemon/' + pokemonID;
+    $.getJSON(url, function (data) {
+        console.log(data);
+
+    })
+};
+
+$(document).ready(function () {
+
+});
